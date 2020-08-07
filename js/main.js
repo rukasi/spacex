c371fa60ae55acbc73dc32942d3452d8d11f39e5
rukasi <fet_nba@mail.ru>
2020-08-07 17:32:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var instance = new vidbg('.video', {
-    mp4: '../img/world.mp4', // URL or relative path to MP4 video
-    webm: '../img/world.webm', // URL or relative path to webm video
-    poster: '../img/poster.jpg', // URL or relative path to fallback image
+    mp4: 'img/world.mp4', // URL or relative path to MP4 video
+    webm: 'img/world.webm', // URL or relative path to webm video
+    poster: 'img/poster.jpg', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   });
 
